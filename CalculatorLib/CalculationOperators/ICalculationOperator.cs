c4d7281fea99816b7533cdd5b32db431388f3c62
Vladimir <fytvova@mail.ru>
2020-03-24 19:24:52 +0300

    Add project files

---
+++ CalculatorLib/CalculationOperators/ICalculationOperator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CalculatorLib.CalculationOperators
+{
+    public interface ICalculationOperator
+    {
+        Dictionary<string, Operator> Operators { get; set; }
+        double DoOperation(double a, double b, Operator @operator);
+        Operator GetOperator(string @operator);
+    }
+}
